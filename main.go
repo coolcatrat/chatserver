a2e8eaa339915ca923b15788f7ad3b9ac12e96ad
coolcatrat <devvratv5@gmail.com>
2026-06-23 03:20:51 +0400

    Added Hub struct to manage all connection
    - newHub(), Hub.add(), Hub.remove(), Hub.broadcast()

--- main.go
+++ main.go
@@ -3,25 +3,62 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
-func handleConn(connection net.Conn) {
+type Hub struct {
+	mutex       sync.Mutex
+	connections map[net.Conn]bool // registery
+}
+
+// methods for Hub
+func newHub() *Hub {
+	return &Hub{connections: make(map[net.Conn]bool)}
+}
+func (hub *Hub) add(connection net.Conn) {
+	hub.mutex.Lock()
+	hub.connections[connection] = true
+	hub.mutex.Unlock()
+}
+func (hub *Hub) remove(connection net.Conn) {
+	hub.mutex.Lock()
+	delete(hub.connections, connection)
+	hub.mutex.Unlock()
+}
+func (hub *Hub) broadcast(sender net.Conn, message []byte) {
+	hub.mutex.Lock()
+	defer hub.mutex.Unlock()
+
+	for connection := range hub.connections {
+		if connection == sender {
+			continue
+		}
+		// add prefix to message to mention sender
+		fmt.Fprintf(connection, "FROM: %s: %s\n", sender.RemoteAddr(), message)
+	}
+}
+
+func handleConn(connection net.Conn, hub *Hub) {
+	// add connection to hub, call the closers to be run after function returns.
 	defer connection.Close()
-	fmt.Println("got a connection from: ", connection.RemoteAddr())
+	hub.add(connection)
+	defer hub.remove(connection)
+	fmt.Println("Connected: ", connection.RemoteAddr())
 
 	buffer := make([]byte, 1024) // buffer for reading bytes from a connection
 	for {
-		n, err := connection.Read(buffer) // read bytes from connection into buffer.
+		bytesRead, err := connection.Read(buffer) // read bytes from connection into buffer.
 		if err != nil {
-			fmt.Println("connection closed: ", connection.RemoteAddr(), err)
+			fmt.Println("disconnected: ", connection.RemoteAddr(), err)
 			return
 		}
-		fmt.Printf("[%s] recieved %d bytes: %q\n", connection.RemoteAddr(), n, buffer[:n])
+		hub.broadcast(connection, buffer[:bytesRead])
+		fmt.Printf("[%s] recieved %d bytes: %q\n", connection.RemoteAddr(), bytesRead, buffer[:bytesRead])
 	}
 }
 
 func main() {
-
+	hub := newHub()
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		fmt.Println("could not listen:", err)
@@ -35,6 +72,6 @@ func main() {
 			fmt.Println("connection failed.", err)
 			continue
 		}
-		go handleConn(newConnection)
+		go handleConn(newConnection, hub)
 	}
 }
